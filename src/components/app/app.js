522397d15710d5a19ef65ca624c6aa6333bb15bf
dgoncharenko <dgoncharenko@crispwp.com>
2020-08-18 12:01:25 +0300

    Base structure

---
+++ src/components/app/app.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './app.css';
+
+import HomePage from '../pages/home-page';
+import CartPage from '../pages/cart-page';
+
+const App = () => {
+	return (
+		<Switch>
+			<Route 
+				path="/"
+				component={HomePage} 
+				exact />
+			<Route 
+				path="/cart"
+				component={CartPage} 
+				/>
+		</Switch>
+	);
+};
+
+export default App;
